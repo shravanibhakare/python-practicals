cf66e8ddc54cee5bbe0a21fc1ce2daa9ccb5e726
shravanibhakare <2022bit048@sggs.ac.in>
2024-07-21 23:11:53 +0530

    basic programe of stating datatypes

---
+++ datatypes.py
@@ -0,0 +1,59 @@
+
+x = "Hello World"
+b = 20
+c = 20.5
+d = 1j
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = range(6)
+h = {"name": "John", "age": 36}
+i = {"apple", "banana", "cherry"}
+j = frozenset({"apple", "banana", "cherry"})
+k = True
+l = b"Hello"
+m = bytearray(5)
+n = memoryview(bytes(5))
+o = None
+p = str("Hello World")
+q = int(20)
+r = float(20.5)
+s = complex(1j)
+t = list(("apple", "banana", "cherry"))
+u = tuple(("apple", "banana", "cherry"))
+v = range(6)
+w = dict(name="John", age=36)
+y = set(("apple", "banana", "cherry"))
+z = bool(5)
+aa = bytes(5)
+ab = bytearray(5)
+ac = memoryview(bytes(5))
+
+print("x:", type(x))
+print("b:", type(b))
+print("c:", type(c))
+print("d:", type(d))
+print("e:", type(e))
+print("f:", type(f))
+print("g:", type(g))
+print("h:", type(h))
+print("i:", type(i))
+print("j:", type(j))
+print("k:", type(k))
+print("l:", type(l))
+print("m:", type(m))
+print("n:", type(n))
+print("o:", type(o))
+print("p:", type(p))
+print("q:", type(q))
+print("r:", type(r))
+print("s:", type(s))
+print("t:", type(t))
+print("u:", type(u))
+print("v:", type(v))
+print("w:", type(w))
+print("y:", type(y))
+print("z:", type(z))
+print("aa:", type(aa))
+print("ab:", type(ab))
+print("ac:", type(ac))
+
